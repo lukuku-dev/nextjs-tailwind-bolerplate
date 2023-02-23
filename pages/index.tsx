fb9542435f61287b655a0b0826cfad8fd50fc570
anjit pariyar <anjitpariyar@gmail.com>
2023-02-23 19:37:55 +0545

    next image config with storyjs and page example added

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,21 +5,26 @@ import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className='flex min-h-screen flex-col items-center justify-center py-2'>
       <Head>
         <title>Tailwind Boilerplate with Storybook</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
+
       <table>
         <tbody>
           <TableRow
-            text="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-            name="Sonang"
-            date="1996-06-11"
+            text='Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+            name='Sonang'
+            date='1996-06-11'
             count={10}
           />
         </tbody>
       </table>
+
+      <footer className='flex h-24 w-full items-center justify-center '>
+        <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
+      </footer>
     </div>
   );
 };
